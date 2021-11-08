365656872927de8dc17853281d92c9a340d21127
oxie <oxie@sfu.ca>
2021-11-08 15:06:39 -0800

    look into argparse

---
+++ parseInput.py
@@ -0,0 +1,16 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    # Add stuff in here
+)
+
+# Arguments:
+# fleet1 - required (int 0 or 1-6)
+# fleet2 - optional (int 0 or 1-6)
+# sub - optional (int 0 or 1-2)
+# stage - required (string or list of strings)
+# hard - optional (flag)
+# heclp - optional (flag)
+# iterations - required (int, at least 1)
+
+# Import procedures and run machine
