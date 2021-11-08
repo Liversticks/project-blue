cad419c051c9afc1e268c79b1f8fca4013033b07
oxie <oxie@sfu.ca>
2021-11-08 15:17:02 -0800

    more options based on current functionality

--- parseInput.py
+++ parseInput.py
@@ -5,6 +5,7 @@ parser = argparse.ArgumentParser(
 )
 
 # Arguments:
+# Farming:
 # fleet1 - required (int 0 or 1-6)
 # fleet2 - optional (int 0 or 1-6)
 # sub - optional (int 0 or 1-2)
@@ -12,5 +13,17 @@ parser = argparse.ArgumentParser(
 # hard - optional (flag)
 # heclp - optional (flag)
 # iterations - required (int, at least 1)
+# qr - optional (flag, will Quick Retire blue and grays using existing settings)
+
+# Cat lodge
+# cat - required
+# buy-box - optional (flag, defaults to false)
+# forts - optional (flag, defaults to true)
+# queue - optional (flag, defaults to true)
+
+# Daily raids
+# raids - required (will do as many as possible, but needs external state)
 
 # Import procedures and run machine
+args = parser.parser_args()
+print(args)
