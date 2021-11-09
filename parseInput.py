4249f49ba0dc26d62183d8bb25d10393c649cc2c
liversticks <oliverxie19@gmail.com>
2021-11-09 00:23:04 -0800

    normal flow - draft args parsing

--- parseInput.py
+++ parseInput.py
@@ -1,29 +1,56 @@
 import argparse
+import sys
 
-parser = argparse.ArgumentParser(
-    # Add stuff in here
-)
+def main(args):
 
-# Arguments:
-# Farming:
-# fleet1 - required (int 0 or 1-6)
-# fleet2 - optional (int 0 or 1-6)
-# sub - optional (int 0 or 1-2)
-# stage - required (string or list of strings)
-# hard - optional (flag)
-# heclp - optional (flag)
-# iterations - required (int, at least 1)
-# qr - optional (flag, will Quick Retire blue and grays using existing settings)
+    parser = argparse.ArgumentParser(
+        description='Automatic AL grinding using a state machine.'
+    )
 
-# Cat lodge
-# cat - required
-# buy-box - optional (flag, defaults to false)
-# forts - optional (flag, defaults to true)
-# queue - optional (flag, defaults to true)
+    # stage - required (string or list of strings)
+    parser.add_argument('stage',
+        help='Stage code. Use <command> to view available stages.'
+    )
 
-# Daily raids
-# raids - required (will do as many as possible, but needs external state)
+    # iterations - required (int, at least 1)
+    parser.add_argument('iterations', default=1, type=int,
+        help='Number of times to repeat stage (must be at least 1)'
+    )
 
-# Import procedures and run machine
-args = parser.parser_args()
-print(args)
+    # fleet1 - required (int 0 or 1-6). Parse args first, validate later
+    parser.add_argument('-f1', '--fleet1', nargs=1, required=True, default=1, type=int,
+        help='First fleet number (integer from 1 to 6 for Normal Mode stages)'
+    )
+    # fleet2 - optional (int 0 or 1-6)
+    parser.add_argument('-f2', '--fleet2', nargs=1, default=2, type=int,
+        help='Second fleet number (integer from 1 to 6 for Normal Mode stages). Cannot be the same as -f1/--fleet1.'
+    )
+    # sub - optional (int 0 or 1-2)
+    parser.add_argument('-s', '--sub', nargs=1, default=1, type=int,
+        help='Submarine fleet number (1 or 2 for Normal Mode stages)'
+    )
+
+    # hard - optional (flag)
+
+    # heclp - optional (flag)
+    parser.add_argument('-he', '--heclp', action='store_true',
+        help='Set a High-Efficiency Combat Logistics Plan for all clears.'
+    )
+
+    # qr - optional (flag, will Quick Retire blue and grays using existing settings)
+
+    # Cat lodge
+    # cat - required
+    # buy-box - optional (flag, defaults to false)
+    # forts - optional (flag, defaults to true)
+    # queue - optional (flag, defaults to true)
+
+    # Daily raids
+    # raids - required (will do as many as possible, but needs external state)
+
+    # Import procedures and run machine
+    a = parser.parse_args(args[1:])
+    print(a)
+
+if __name__ == '__main__':
+    main(sys.argv)
