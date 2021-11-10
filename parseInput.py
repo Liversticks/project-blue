554927678791491d26e1d0d585ca0d3ca74ed277
liversticks <oliverxie19@gmail.com>
2021-11-09 16:42:42 -0800

    stage and fleet validation

--- parseInput.py
+++ parseInput.py
@@ -1,24 +1,37 @@
 import argparse
 import sys
+import re
+from mss import mss
 
-def main(args):
-    modes = ['battle', 'cat']
-    
-    parser = argparse.ArgumentParser(
-        description='Automatic AL grinding using a state machine.'
-    )
-    parser.add_argument('mode',
-        choices=modes,
-        help='Supported modes: %(choices)s'
-    )
+# TODO: rewrite using subparsers
 
-    preliminary = parser.parse_args(args[0:1])
-    if (preliminary.mode == 'battle'):
-        campaign_or_event(args[1:])
-    else:
-        cat_lodge(args[1:])
+stage_re = re.compile('event(-hard)?|1[0-4]-[1-4]|[1-9]-[1-4]|[A-D][1-4]')
 
-def cat_lodge(args):
+class InvalidStageError(Exception): pass
+
+def validate_and_split_stages(stage_string):
+    stages = stage_string.split()
+    for stage in stages:
+        match = stage_re.match(stage)
+        if match is None:
+            raise InvalidStageError
+    return stages
+
+class FleetsCannotBeSameError(Exception): pass
+
+class FleetOutOfRangeError(Exception): pass
+
+def validate_surface_fleets(fleet1, fleet2, is_hard=False):
+    if not is_hard:
+        if fleet1 == fleet2:
+            raise FleetsCannotBeSameError
+        if fleet1 < 1 or fleet1 > 6:
+            raise FleetOutOfRangeError
+        if fleet2 is not None:
+            if fleet2[0] < 1 or fleet2[0] > 6:
+                raise FleetOutOfRangeError
+
+def parse_cat_lodge(args):
     parser = argparse.ArgumentParser(
         prog=f'{sys.argv[0]} cat',
         description='Cat lodge submenu: handles Meowfficer and cat lodge tasks.'
@@ -42,8 +55,7 @@ def cat_lodge(args):
     a = parser.parse_args(args)
     print(a)
 
-
-def campaign_or_event(args):
+def parse_campaign_or_event(args):
     parser = argparse.ArgumentParser(
         prog=f'{sys.argv[0]} battle',
         description='Battle submenu: handles clearing campaign and event stages.'
@@ -81,14 +93,52 @@ def campaign_or_event(args):
 
     # qr - optional (flag, will Quick Retire blue and grays using existing settings)
 
+    parsed_args = parser.parse_args(args)
+    print(parsed_args)
 
+    try:
+        split_stages = validate_and_split_stages(parsed_args.stage)
+        print(split_stages)
+        validate_surface_fleets(parsed_args.fleet1[0], parsed_args.fleet2)
+    except InvalidStageError:
+        print("The specified stage is invalid.")
+        print("List of accepted stages:")
+        print()
+        print("EVENT STAGES:")
+        print("event - Normal Mode event stages")
+        print("event-hard - Hard Mode event stages")
+        print("X-Y : X = Chapter number (A, B, C, D); Y = stage number (1 - 3)")
+        print()
+        print("NORMAL STAGES:")
+        print("X-Y : X = Chapter number (1 - 14); Y = stage number (1 - 4)")
+        return
+    except FleetsCannotBeSameError:
+        print("Surface fleets cannot be the same (example: cannot have fleet1 = 1, fleet2 = 1).")
+        return
+    except FleetOutOfRangeError:
+        print("For Normal Mode, the value for each surface fleet must be an integer between 1 and 6 (inclusive).")
+        return
 
-    # Daily raids
-    # raids - required (will do as many as possible, but needs external state)
+def main(args):
+    modes = ['battle', 'cat', 'raid']
+    
+    parser = argparse.ArgumentParser(
+        description='Automatic AL grinding using a state machine.'
+    )
+    parser.add_argument('mode',
+        choices=modes,
+        help='Supported modes: %(choices)s'
+    )
 
-    # Import procedures and run machine
-    a = parser.parse_args(args)
-    print(a)
+    preliminary = parser.parse_args(args[0:1])
+    if (preliminary.mode == 'battle'):
+        parse_campaign_or_event(args[1:])
+    elif (preliminary.mode == 'cat'):
+        parse_cat_lodge(args[1:])
+    else:
+        # Daily raids
+        # raids - required (will do as many as possible, but needs external state)
+        print('Doing daily raids')
 
 if __name__ == '__main__':
     main(sys.argv[1:])
