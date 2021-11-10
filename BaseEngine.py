6ce783c1192ed79ef8959beb72882eb819ab2ecc
liversticks <oliverxie19@gmail.com>
2021-11-09 20:58:39 -0800

    base class for window manager and state machine instances

---
+++ BaseEngine.py
@@ -0,0 +1,12 @@
+from windowManager import WindowMgr
+from StateMachine import StateMachine
+
+class BaseEngine():
+
+    def __init__(self, sct):
+        self.wm = WindowMgr()
+        self.wm.find_window_wildcard("BlueStacks")
+        self.wm.set_foreground()
+        self.machine = StateMachine(self.wm, sct)
+        
+
