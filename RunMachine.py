69b2c911869f9abf6febade20a9e67b2b4c4d3ff
liversticks <oliverxie19@gmail.com>
2021-11-14 16:36:19 -0800

    add raids option

--- RunMachine.py
+++ RunMachine.py
@@ -1,11 +1,17 @@
 from BattleEngine import BattleEngine
 from CatEngine import CatEngine
+from RaidEngine import RaidEngine
 from mss import mss
 
 def run_battle(options):
     with mss() as sct:
         be = BattleEngine(sct)
         be.clear_stage(options)
+        be.cleanup()
 
 def run_cat(options):
     print()
+
+def run_raid(options):
+    re = RaidEngine(None)
+    re.run_raid(options)
