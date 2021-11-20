906a7631867ec42f9d2dc89bb30505c8e85c91c7
liversticks <oliverxie19@gmail.com>
2021-11-19 21:04:30 -0800

    start unit tests

---
+++ tests/main.py
@@ -0,0 +1,37 @@
+import unittest
+import machine.StateMachine as m
+import transitions
+
+class TestStateMachine(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        # Do not call any stage clear transition or one that relies on time_and_screenshot!
+        self.machine = m.StateMachine(None, None)
+
+    def test_start(self):
+        self.assertEqual(self.machine.state, 'main-menu')
+
+    def test_main_menu_fail(self):
+        with self.assertRaises(transitions.core.MachineError):
+            self.machine.to_exercises()
+        
+        with self.assertRaises(transitions.core.MachineError):
+            self.machine.to_cattery()
+
+        with self.assertRaises(transitions.core.MachineError):
+            self.machine.to_daily_raids()
+
+        with self.assertRaises(transitions.core.MachineError):
+            self.machine.to_campaign()
+
+    def test_main_menu_hq(self):
+        self.machine.to_hq()
+        self.assertEqual(self.machine.state, 'hq')
+
+    def test_main_menu_battle(self):
+        self.machine.to_battle()
+        self.assertEqual(self.machine.state, 'battle')
+
+if __name__ == '__main__':
+    unittest.main()
